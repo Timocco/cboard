de3d7b18f2d685e3ebfcacc66c503b92e5cae79e
Joshua Moyers <josh@restoreskills.com>
2022-05-04 11:20:20 -0400

    Pipeline buildspec and env set up

--- env/prod.js
+++ env/prod.js
@@ -1,11 +1,11 @@
-/* 
+/*
  * PROD CONFIG
  * This is intended for deployment to a hosting service.
  */
 
-const APP_URL = 'https://cboard.somehost.com';
+const APP_URL = 'https://cboard.restoreskills.com';
 
 module.exports = {
   APP_URL,
-  EXAMPLE_CONFIG: 'Prod config',
+  EXAMPLE_CONFIG: 'Prod config'
 };
